62cef8394a4cd0b251d87d10e6dcebe70b779418
Lasha Loladze <l.loladze11@gmail.com>
2021-07-26 19:51:40 +0400

    Added clearing listener on Destroy.

--- src/app/responsive.directive.ts
+++ src/app/responsive.directive.ts
@@ -2,37 +2,44 @@ import { Directive, Renderer2, Input, TemplateRef, ViewContainerRef } from '@ang
 import { screenSizes } from './responsive.interface';
 
 @Directive({
-  selector: '[appResponsive]'
+  selector: '[appResponsive]',
 })
 export class ResponsiveDirective {
-
   private _screenSize: screenSizes = 'sm';
-  @Input('appResponsive') 
+  @Input('appResponsive')
   set permissionIf(value: screenSizes) {
-    this._screenSize=value;
+    this._screenSize = value;
     this.updateView(window.innerWidth);
-    this.renderer.listen(window, 'resize', this.onResize.bind(this)); 
   }
 
-  BREAKPOINTS = {
-    "md": 960,
-    "lg": 1280,
-  }
+  // ListenerFn to remove listener on Destroy
+  listenerFn: () => void;
+
+  breakPoints = {
+    md: 960,
+    lg: 1280,
+  };
 
   viewExists: boolean = false;
 
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainerRef: ViewContainerRef,
-    private renderer: Renderer2,
-  ) { }
+    private renderer: Renderer2
+  ) {
+    this.listenerFn = this.renderer.listen(
+      window,
+      'resize',
+      this.onResize.bind(this)
+    );
+  }
 
-  onResize(event: any): void{
+  onResize(event: any): void {
     this.updateView(event.target.innerWidth);
   }
 
-  private updateView(width: number): void{
-    if(this.checkVisibility(width) && !this.viewExists) {
+  private updateView(width: number): void {
+    if (this.checkVisibility(width) && !this.viewExists) {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
       this.viewExists = true;
     } else if (!this.checkVisibility(width) && this.viewExists) {
@@ -41,16 +48,22 @@ export class ResponsiveDirective {
     }
   }
 
-  private checkVisibility(width: number): boolean{
-    switch(this._screenSize) {
+  private checkVisibility(width: number): boolean {
+    switch (this._screenSize) {
       case 'sm': {
-        return width < this.BREAKPOINTS.md;
+        return width < this.breakPoints.md;
       }
       case 'md': {
-        return width < this.BREAKPOINTS.lg && width >= this.BREAKPOINTS.md;
+        return width < this.breakPoints.lg && width >= this.breakPoints.md;
       }
-      default: return width >= this.BREAKPOINTS.lg;
+      default:
+        return width >= this.breakPoints.lg;
     }
   }
 
+  ngOnDestroy() {
+    if (this.listenerFn) {
+      this.listenerFn();
+    }
+  }
 }
